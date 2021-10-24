45d32c7fbffad30aaf5b62c933f54258f45bd05d
Adam Slabadack <arslabadack@gmail.com>
2021-10-23 22:34:09 -0300

    started DB models

--- okmoney_app/models.py
+++ okmoney_app/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 
-# exemplo de implementação de uma classe produto no bando de dados
 
-
-class Produto(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
-    estque = models.IntegerField('Quantidade em estoque')
-# classe que exibe os nomes no admin
+class MoneyIn(models.Model):
+    categoria = models.CharField(max_length=100)
+    valor = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    observacao = models.TextField(max_length=200, blank=True)
 
     def __str__(self):
         return self.nome
+
+
+class MoneyOut(models.Model):
+    categoria = models.CharField('Categoria', max_length=100)
+    motivo = models.CharField('Motivo', max_length=200)
+    local = models.CharField('Local', max_length=200)
+    valor = models.DecimalField('Valor', decimal_places=2, max_digits=8)
+    data = models.DateField('Data')
+    metodo_pagamento = models.models.BooleanField(("Método de Pagamento"))
+    observacao = models.TextField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.motivo
+
+
+class reminders(models.Model):
+    lembrete = models.CharField('Lembrete', max_length=500, blank=True)
+
+    def __str__(self):
+        return self.lembrete
